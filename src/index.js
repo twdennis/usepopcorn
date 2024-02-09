1f56478ae8aec906c0b365127926992397f937d4
Tom <131278560+twdennis@users.noreply.github.com>
2024-02-09 01:31:11 +0000

    restored to original app function

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import './index.css';
-// import App from './App';
+import "./index.css";
+import App from "./App";
 
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-
-  return (
-    <>
-      <StarRating maxRating={7} onSetRating={setMovieRating}/>
-      <p>{movieRating? `You have given this movie a rating of ${movieRating}` : "Give this movie a rating!"}</p>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <StarRating maxRating={7} onSetRating={setMovieRating}/>
+//       <p>{movieRating? `You have given this movie a rating of ${movieRating}` : "Give this movie a rating!"}</p>
+//     </>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating />
+    <App />
+    {/* <StarRating />
     <StarRating
       size={24}
       color="blue"
@@ -29,6 +28,6 @@ root.render(
       messages={["awful", "not good", "ok", "pretty good", "great"]}
       defaultRating={3}
     />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
