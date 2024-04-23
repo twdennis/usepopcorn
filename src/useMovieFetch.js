8c70c801250c9974a2f80eef85f6afaf2a6c222c
Tom <131278560+twdennis@users.noreply.github.com>
2024-04-23 15:40:54 +0100

    changed api call to https

--- src/useMovieFetch.js
+++ src/useMovieFetch.js
@@ -16,7 +16,7 @@ export function useMovieFetch(query, callback) {
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
